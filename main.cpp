94716ef9dc9c88ef3c6c90a4ea34c529aeb73b3b
ahmed-elsafty16005023 <67247668+ahmed-elsafty16005023@users.noreply.github.com>
2025-11-11 09:58:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,9 @@ using namespace std;
 
 // ==================== Person Class Implementation =========================
 
-
+void Person::display() {
+    cout << getName() << " (ID: " << getId() << ")" << endl;
+}
 
 
 
@@ -19,21 +21,48 @@ using namespace std;
 
 // ==================== Student Class Implementation ====================
 
-
+void Student::display() {
+    cout << "Student Info:" << endl;
+    cout << "Name: " << getName() << endl;
+    cout << "Year: " << yearLevel << endl;
+    cout << "Major: " << major << endl;
+}
 
 
 
 
 // ==================== Instructor Class Implementation ====================
 
-
+void Instructor::display() {
+    cout << "Instructor Info:" << endl;
+    cout << "Name: " << getName() << endl;
+    cout << "Department: " << department << endl;
+    cout << "Experience: " << experienceYears << " years" << endl;
+}
 
 
 
 
 // ==================== Course Class Implementation ====================
 
+void Course::addStudent(const Student&s) {
+    if (currentStudents == maxStudents) {
+        cout << "The maximum number of students to be enrolled in this course have been reached, you cannot be enrolled in this course" << endl;
+        return;
+    }
 
+    students[currentStudents] = s;
+    currentStudents++;
+}
+
+void Course::displayCourseInfo() {
+    cout << "Course: " << courseCode << " -" << courseName;
+    cout << "Max Students: " << maxStudents;
+    cout << "Currently Enrolled: ";
+    for (int i = 0; i < currentStudents; i++) {
+        students[i].display();
+    }
+}
 
 
 
@@ -42,7 +71,17 @@ using namespace std;
 
 // ==================== Main Function ====================
 int main() {
-   
-    
+    Course* course = new Course("CS101", "Introduction to Programming", 3);
+    Student* s1 = new Student("Omar Nabil", 2202, 2, "Informatics");
+    Instructor* i1 = new Instructor("Dr. Lina Khaled", 1002, "Computer Science", 5);
+
+    course->addStudent(*s1);
+
+    course->displayCourseInfo();
+
+    i1->display();
+
+    s1->display();
+
     return 0;
 }

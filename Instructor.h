7b7e062ebb8954b3ad2ce792897b65a93cfb671d
ahmed-elsafty16005023 <67247668+ahmed-elsafty16005023@users.noreply.github.com>
2025-11-11 09:56:22 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -3,7 +3,33 @@
 
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
+class Person {
+private:
+    string name;
+    int id;
+
+public:
+    Person(string name, int id) {
+        this->name = name;
+        this->id = id;
+    }
+    Person() {
+        name = "none";
+        id = 0;
+    }
+    ~Person() {
+
+    }
+
+    string getName() {
+        return name;
+    }
+    int getId() {
+        return id;
+    }
+
+    void display();
+};
 
 
 

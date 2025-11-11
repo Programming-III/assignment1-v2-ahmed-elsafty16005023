a233cf42546a0343a823a960c7b89af952a7b255
ahmed-elsafty16005023 <67247668+ahmed-elsafty16005023@users.noreply.github.com>
2025-11-11 09:55:18 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -4,7 +4,38 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
+class Course{
+private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+
+public:
+    Course(string courseCode, string courseName, int maxStudents) {
+        this->courseCode = courseCode;
+        this->courseName = courseName;
+        this->maxStudents = maxStudents;
+        this->students = new Student[maxStudents];
+        this->currentStudents = 0;
+    }
+    Course() {
+        this->courseCode = "0";
+        this->courseName = "NAN";
+        this->maxStudents = 0;
+    }
+
+    ~Course() {
+        delete [] students;
+    }
+
+    int getMaxStudents() {
+        return maxStudents;
+    }
+    void addStudent(const Student& s);
+    void displayCourseInfo();
+};
 
 
 

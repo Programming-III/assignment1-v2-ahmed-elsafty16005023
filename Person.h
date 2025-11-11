3afcf8eedbf224eaa1aaa5ce967e039ff2a9b7c6
ahmed-elsafty16005023 <67247668+ahmed-elsafty16005023@users.noreply.github.com>
2025-11-11 09:56:56 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -4,7 +4,33 @@
 #include <string>
 using namespace std;
 
-//#write Person class here
+class Person {
+private:
+    string name;
+    int id;
+
+public:
+    Person(string name, int id) {
+        this->name = name;
+        this->id = id;
+    }
+    Person() {
+        name = "none";
+        id = 0;
+    }
+    ~Person() {
+
+    }
+
+    string getName() {
+        return name;
+    }
+    int getId() {
+        return id;
+    }
+
+    void display();
+};
 
 
 
